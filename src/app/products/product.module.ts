c97dd884d3ae528150cc256e66ad34b16806ea6e
ramazan.alp <ramazan.alp@pia-team.com>
2021-10-07 12:03:04 +0100

    product-edit delete

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -12,6 +12,8 @@ import { SharedModule } from '../shared/shared.module';
     SharedModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', component: ProductDetailComponent },
+      { path: 'products/:id/edit', component: ProductEditComponent },
     ]),
   ],
   declarations: [
